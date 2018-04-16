4bc7e138559ce1065404856f64cccbdbb0d31f31
Johan Wastring <jwr@SELUN-JWR-MAC1.lan>
2018-04-16 22:16:56 +0200

    Commiting for Heroku. Start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 //const comm = require('./views/commands');
 // an idea would be to precalculate the commands before sending them as 
 // parameters to the page when showing it.
@@ -106,6 +108,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is up and running at port 3000');
+app.listen(port, () => {
+    console.log(`Server is up and running at port ${port}`);
 });
